aa34289af658bb45a311d1eba09b61f37737f565
Caroh2006 <leah.githaiga@strathmore.edu>
2026-02-24 14:37:47 +0300

    test<3

---
+++ src/person.java
@@ -0,0 +1,22 @@
+public abstract class person {
+    private String name;
+    private int id;
+    
+public person(String name, int id) {
+    this.name = name;
+    this.id = id;
+}
+public String getname() {
+    return name;
+}
+public int getid() {
+    return id;
+}
+public abstract void displayrole();
+
+}
+
+
+
+
+
