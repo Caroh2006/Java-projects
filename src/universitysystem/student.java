08b2167742d1319b05f6e6fea635dde7e46732f8
Caroh2006 <leah.githaiga@strathmore.edu>
2026-02-26 02:02:13 +0300

    Update student.java

--- src/universitysystem/student.java
+++ src/universitysystem/student.java
@@ -1,9 +1,9 @@
 package universitysystem;
 
-class Student extends Person implements Registration, FeePayment {
+public class student extends person implements Registration, FeePayment {
     private String program;
 
-    public Student(String name, String id, String program) {
+    public student(String name, String id, String program) {
         super(name, id);
         this.program = program;
     }
@@ -12,39 +12,20 @@ class Student extends Person implements Registration, FeePayment {
     public void registerCourse(String courseName) {
         System.out.println(getName() + " enrolled in " + courseName);
     }
-
     @Override
     public void payFee(double amount) {
-        System.out.println(getName() + " paid fee: " + amount);
+        System.out.println(getName() + " paid fee:" + amount);
     }
 
-    @Override
-    public void displayRole() {
-        System.out.println("I am a Student.");
-    }age universitysystem;
-
-class Student extends Person implements Registration, FeePayment {
-    private String program;
-
-    public Student(String name, String id, String program) {
-        super(name, id);
-        this.program = program;
-    }
-
-    @Override
-    public void registerCourse(String courseName) {
-        System.out.println(getName() + " enrolled in " + courseName);
-    }
 
     @Override
     public void displayrole() {
         System.out.println("I am a Student");
     }
-}
-    public String getprogram() {
+    public String getProgram() {
         return program;
-}
-    public void setprogram(String program) {
+    }
+    public void setProgram(String program) {
         this.program = program;
     }
 }
