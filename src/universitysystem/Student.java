245ef7a835a262087c577851dacd5afd1835d37e
Caroh2006 <leah.githaiga@strathmore.edu>
2026-03-31 13:20:10 +0300

    Update Student.java

--- src/universitysystem/Student.java
+++ src/universitysystem/Student.java
@@ -1,20 +1,25 @@
 package universitysystem;
 
+import java.util.ArrayList;
+
 public class Student extends Person implements Registration, FeePayment {
+
     private String program;
+    private ArrayList<String> courses = new ArrayList<>();
 
     public Student(String name, String id, String program) {
         super(name, id);
-        this.program = program;;
+        this.program = program;
     }
 
     @Override
-     public void displayRole() {
+    public void displayRole() {
         System.out.println("Role: Student in program " + program);
     }
 
     @Override
     public void registerCourse(String courseName) {
+        courses.add(courseName);
         System.out.println("Registered for course: " + courseName);
     }
 
@@ -22,5 +27,12 @@ public class Student extends Person implements Registration, FeePayment {
     public void payFee(double amount) {
         System.out.println(getName() + " paid fee: $" + amount);
     }
-    
+
+    public String getProgram() {
+        return program;
+    }
+
+    public ArrayList<String> getCourses() {
+        return courses;
+    }
 }
